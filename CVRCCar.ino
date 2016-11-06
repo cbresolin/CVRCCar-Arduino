79304c78a0656d131049c45e6d4743755fcac137
CHRISTOPHE BRESOLIN <christophe.bresolin@gmail.com>
2016-11-06 22:31:53 +0100

    Control Servo from json

--- CVRCCar.ino
+++ CVRCCar.ino
@@ -13,7 +13,6 @@
 #define MAXLEFT 1370 // 71°
 #define STEERINGRESPONSE 10
 Servo steeringservo;
-int steering = STRAIGHT;
 
 #define THROTTLEPIN 10
 #define NEUTRAL 1490 // 86°
@@ -23,7 +22,6 @@ int steering = STRAIGHT;
 #define MINTHROTTLE 1200 // 61°
 #define THROTTLERESPONSE 10
 Servo throttleservo;
-int throttle = NEUTRAL;
 
 StaticJsonBuffer<100> jsonBuffer;
 
@@ -34,59 +32,27 @@ void setup()
   
   // attaches steering servo
   steeringservo.attach(STEERINGPIN);
-  writeservo(steeringservo, steering);
+  set_steering(STRAIGHT);
 
   // attaches throttle servo
   throttleservo.attach(THROTTLEPIN);
-  writeservo(throttleservo, throttle);
+  set_throttle(NEUTRAL);
 }
 
-void steer_right()
+void set_steering(int pwm)
 { 
-  if (steering < MAXRIGHT)
-  {
-    steering += STEERINGRESPONSE;
-    writeservo(steeringservo, steering);
-  }
+  writeservo(steeringservo, pwm);
 }
 
-void steer_left()
+void set_throttle(int pwm)
 {
-  if (steering > MAXLEFT)
-  {
-    steering -= STEERINGRESPONSE;
-    writeservo(steeringservo, steering);
-  }
-  
-}
-
-void throttle_up()
-{
-  if (throttle < MAXTHROTTLE)
-  {
-    throttle += THROTTLERESPONSE;
-    writeservo(throttleservo, throttle);
-  }
-}
-
-void throttle_down()
-{
-  if (throttle > MINTHROTTLE)
-  {
-    throttle -= THROTTLERESPONSE;
-    writeservo(throttleservo, throttle);
-  }
+  writeservo(throttleservo, pwm);
 }
 
 void writeservo(Servo servo, int time)
 {
   servo.writeMicroseconds(time);
   delay(15);
-}
-
-int readservo(Servo servo)
-{
-  return servo.read();
 }
 
 // the loop routine runs over and over again forever:
@@ -97,12 +63,14 @@ void loop()
       char json_buffer[60] = "";
       int pan;
       int tilt;
+      int steering;
+      int throttle;
 
       // If anything comes in Serial (USB)
       Serial.readBytesUntil("#", json_buffer, 60);
       
       // Test if parsing succeeds.
-      // Example: {"pan":1543,"tilt":1774,"throttle":1390,"steering":1910}#
+      // Example: {"pan":1543,"tilt":1774,"throttle":1490,"steering":1640}#
       JsonObject& root = jsonBuffer.parseObject(json_buffer);
       if (!root.success())
       {
@@ -116,36 +84,13 @@ void loop()
         tilt = root["tilt"];
         throttle = root["throttle"];
         steering = root["steering"];
-      }  
-      
-//      switch (input)
-//      {
-//        case '4':
-//          steer_left();
-//          break;
-//        case '6':
-//          steer_right();
-//          break;
-//          case '-':
-//          throttle_down();
-//          break;
-//        case '+':
-//          throttle_up();
-//          break;
-//        default: 
-//          // Nothing
-//        break;
-//      }
-      
-      // Debug
-      Serial.print("Pan = ");
-      Serial.print(pan);
-      Serial.print(", Tilt = ");
-      Serial.print(tilt);
-      Serial.print(", Steering = ");
-      Serial.print(steering);
-      Serial.print(", Throttle = ");
-      Serial.println(throttle);
+      }
+
+      // Act on steering
+      set_steering(steering);
+
+      // Act on throttle
+      set_throttle(throttle);
     }
 }
 
