6e60010ba5c238be7360420d6284d53f16c343d0
CHRISTOPHE BRESOLIN <christophe.bresolin@gmail.com>
2017-01-18 19:05:01 +0100

    Handle UltraSound measurements
    - change throttle pin due to timer conflicts
    - use of HC-SR04 to measure obstacle distance

--- CVRCCar-Arduino.ino
+++ CVRCCar-Arduino.ino
@@ -1,11 +1,17 @@
-/*
- Controlling a servo position using serial inputs
-*/
-
 #include <Servo.h>
+#include <NewPing.h>
 #include <ArduinoJson.h>
 
 #define SERIALBAUDRATE 9600
+#define TRIGGERPIN 4
+#define ECHOPIN 7
+#define MAXDISTANCE 150 // 1.5 meters
+#define STOPDISTANCE 15 // 15 centimeters
+
+NewPing sonar(TRIGGERPIN, ECHOPIN, MAXDISTANCE);
+
+unsigned int pingSpeed = 500; // How frequently are we going to send out a ping (in milliseconds). 50ms would be 20 times a second.
+unsigned long pingTimer;       // Holds the next ping time.
 
 #define PANPIN 9
 #define PANSTRAIGHT 1385 // 82°
@@ -15,7 +21,7 @@ Servo panservo;
 #define STRAIGHT 1640 // 105°
 Servo steeringservo;
 
-#define THROTTLEPIN 11
+#define THROTTLEPIN 12
 #define NEUTRAL 1490 // 86°
 Servo throttleservo;
 
@@ -35,6 +41,8 @@ void setup()
   // attaches throttle servo
   throttleservo.attach(THROTTLEPIN, 1370, 1560);
   set_throttle(NEUTRAL);
+
+  pingTimer = millis(); // Start now.
 }
 
 void set_pan(int pwm)
@@ -58,9 +66,24 @@ void writeservo(Servo servo, int time)
   delay(15);
 }
 
+void echoCheck()
+{ // Timer2 interrupt calls this function every 24uS where you can check the ping status.
+  if (sonar.check_timer())
+  { // This is how you check to see if the ping was received.
+    int unsigned dist = NewPingConvert(sonar.ping_result, US_ROUNDTRIP_CM);
+    Serial.println(dist); // Ping returned, uS result in ping_result, convert to cm with US_ROUNDTRIP_CM.
+  }
+}
+
 // the loop routine runs over and over again forever:
 void loop()
 {
+  // Notice how there's no delays in this sketch to allow you to do other processing in-line while doing distance pings.
+  if (millis() >= pingTimer) {   // pingSpeed milliseconds since last ping, do another ping.
+    pingTimer += pingSpeed;      // Set the next ping time.
+    sonar.ping_timer(echoCheck); // Send out the ping, calls "echoCheck" function every 24uS where you can check the ping status.
+  }
+  
   if (Serial.available())
   {
     char serialbuffer[45] = "";
